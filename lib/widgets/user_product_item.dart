0466c8ffb26c9345b622917f654fc33607d5dbbd
Frabbi727 <Frabbi727@gmail.com>
2021-12-26 00:10:48 +0600

    Delete function work perfectly, and it also deleted for the main screen.

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
+import '../providers/product_provider.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -25,13 +27,16 @@ class UserProductItem extends StatelessWidget {
           children: <Widget>[
             IconButton(
               onPressed: () {
-                Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments:id );
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
               },
               icon: Icon(Icons.edit),
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
             )
